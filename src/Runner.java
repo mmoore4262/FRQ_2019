08bac14e007cf46c3cb16c38feb70cb8f1dddb3f
f <g>
2019-05-20 10:01:44 -0400

    init

--- src/Runner.java
+++ src/Runner.java
@@ -1,7 +1,17 @@
+import java.util.ArrayList;
 
 public class Runner{
     public static void main (String[] args) {
-        
+        Delimiters hey= new Delimiters("</c","</o>");
+        String[] tokens= {"</c","not", "open", "</c", "</c", "</o>", "</o>", "</o>"};
+
+        System.out.println(hey.getDelimitersList(tokens));
+        ArrayList<String> k=new ArrayList<>();
+        for (int i=0; i<tokens.length; i++)
+        {
+            k.add(tokens[i]);
+        }
+        System.out.println(hey.isBalanced(k));
     }
 }
 
