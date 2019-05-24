073b5c0bb79dda633a4346d9851369f320af047c
f <g>
2019-05-24 09:42:15 -0400

    matt

--- src/APCalendar.java
+++ src/APCalendar.java
@@ -32,6 +32,6 @@ public class APCalendar
     }
     public static int dayOfWeek(int month, int day, int year)
     {
-        return ((firstDayOfYear(year) + dayOfYear(month, day, year)) - 1) % 7;
+        return ((firstDayOfYear(year) + dayOfYear(month, day, year)) - 2) % 7;
     }
 }
